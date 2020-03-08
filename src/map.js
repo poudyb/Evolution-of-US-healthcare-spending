9860aa8ed33466e32b6915076009debcb8297147
bidusha <bidusha@mit.edu>
2020-03-07 20:48:58 -0500

    Add files via upload

---
+++ src/map.js
@@ -0,0 +1,20 @@
+
+var width="960", height="600"
+ var svg = d3.select("body").append("svg").attr("width", width).attr("height",height);
+
+var path = d3.geoPath();
+
+d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+  if (error) throw error;
+
+  svg.append("g")
+      .attr("class", "states")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter().append("path")
+      .attr("d", path);
+
+  svg.append("path")
+      .attr("class", "state-borders")
+      .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
+});
