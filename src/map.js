1d5a1bc97a53a813edf4bd3c717c78c57c3df582
bidusha <bidusha@mit.edu>
2020-03-26 13:52:11 -0400

    Add files via upload

--- src/map.js
+++ src/map.js
@@ -1,20 +1,48 @@
+var width="960", height="600";
+var svg = d3.select("body").append("svg").attr("width", width).attr("height",height);
 
-var width="960", height="600"
- var svg = d3.select("body").append("svg").attr("width", width).attr("height",height);
 
 var path = d3.geoPath();
 
-d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/states-albers-10m.json", function(error, us) {
   if (error) throw error;
 
   svg.append("g")
-      .attr("class", "states")
+    .attr("class", "states")
     .selectAll("path")
     .data(topojson.feature(us, us.objects.states).features)
     .enter().append("path")
-      .attr("d", path);
+    .attr("d", path);
 
   svg.append("path")
       .attr("class", "state-borders")
-      .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
+      .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))); 
+
+  svg.append("g")
+    .attr("class", "states")
+    .selectAll("text")
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter().append("text")
+    .attr("transform", function (d) { return "translate(" + path.centroid(d) + ")"; })
+    .attr("dx", function (d) { return d.properties.dx || "0"; })
+    .attr("dy", function (d) { return d.properties.dy || "0.20em"; })
+    .text(function (d) { return d.properties.name; })
+    .attr('font-size','6.5pt')
+    .attr("text-anchor","middle")
+    .style('fill', 'darkOrange');
+ 
 });
+
+function update(h) {
+    // update position and text of label according to slider scale
+    handle.attr("cx", x(h));
+    label
+        .attr("x", x(h))
+        .text(formatDateIntoYear(h));
+
+    //filter data set and redraw plot
+    var newData = dataset.filter(function(d) {
+        return d.date < h;
+    })
+    //drawHeatMap(newData);
+}
--- src/slideBar.js
+++ src/slideBar.js
@@ -26,9 +26,9 @@ slider.append("line")
     .attr("class", "track")
     .attr("x1", x.range()[0])
     .attr("x2", x.range()[1])
-  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
     .attr("class", "track-inset")
-  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
     .attr("class", "track-overlay")
     .call(d3.drag()
         .on("start.interrupt", function() { slider.interrupt(); })
@@ -36,8 +36,8 @@ slider.append("line")
 
 slider.insert("g", ".track-overlay")
     .attr("class", "ticks")
-    .attr("transform", "translate(0," + 18 + ")")
-  .selectAll("text")
+    .attr("transform", "translate(0," + 25 + ")")
+    .selectAll("text")
     .data(x.ticks(5))
     .enter()
     .append("text")
@@ -50,15 +50,10 @@ var label = slider.append("text")
     .attr("class", "label")
     .attr("text-anchor", "middle")
     .text(formatDateIntoYear(startDate))
-    .attr("transform", "translate(0," + (-25) + ")")
+    .attr("transform", "translate(0," + (-30) + ")")
 
-var handle = slider.insert("circle", ".track-overlay")
+
+ var handle = slider.insert("circle", ".track-overlay")
     .attr("class", "handle")
     .attr("r", 9);
 
-function update(h) {
-  handle.attr("cx", x(h));
-  label
-    .attr("x", x(h))
-    .text(formatDateIntoYear(h));
-}
