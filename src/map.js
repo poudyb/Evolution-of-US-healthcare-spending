984cc06533ce5d94e47ae1dfad709dc867c0bdef
bidusha <bidusha@mit.edu>
2020-03-26 20:14:20 -0400

    Add files via upload

--- src/map.js
+++ src/map.js
@@ -4,13 +4,21 @@ var svg = d3.select("body").append("svg").attr("width", width).attr("height",hei
 
 d3.queue()
     .defer(d3.json, "https://cdn.jsdelivr.net/npm/us-atlas@3/states-albers-10m.json") // Loading US States
+    .defer(d3.csv, "/data/StateDataWithTotalSpending.csv")
+    .defer(d3.csv, "/data/StatesWithId.csv")
     .await(ready); // The method 'ready' runs when all the input JSONs are loaded
 
 var path = d3.geoPath();
 
 //Function that runs when the data is loaded
-function ready(error, us) {
+function ready(error, us, spending, statesWithId) {
   if (error) throw error;
+    
+    var abbreviatedName = {};
+    statesWithId.forEach(function(d,i){
+      abbreviatedName[d.name] = d.abbreviation;
+    });
+
 
   svg.append("g")
     .attr("class", "states")
@@ -31,8 +39,10 @@ function ready(error, us) {
     .attr("transform", function (d) { return "translate(" + path.centroid(d) + ")"; })
     .attr("dx", function (d) { return d.properties.dx || "0"; })
     .attr("dy", function (d) { return d.properties.dy || "0.20em"; })
-    .text(function (d) { return d.properties.name; })
-    .attr('font-size','6.5pt')
+    .text(function (d) { 
+    console.log(abbreviatedName[d.properties.name])
+    return abbreviatedName[d.properties.name]; })
+    .attr('font-size','8 pt')
     .attr("text-anchor","middle")
     .style('fill', 'darkOrange');
  };
